d06e713f081aab1a549e1a41718b6cb1f7683580
Paul Cowan <dagda1@scotalt.net>
2012-05-08 08:16:57 +0100

    models extended to ember data records

--- app/assets/javascripts/app/models/group.js.coffee
+++ app/assets/javascripts/app/models/group.js.coffee
@@ -1,1 +1,2 @@
 WZ.Group = DS.Model.extend WZ.Helpers.Common,
+  name: DS.attr 'string'
--- app/assets/javascripts/vendor.js
+++ app/assets/javascripts/vendor.js
@@ -1,6 +1,6 @@
 //= require vendor/json2.js
 //= require vendor/ember.js
 //= require vendor/ember-routemanager.js
-//= require vendor/ember-data.jsember-routemanager.js
+//= require vendor/ember-data.js
 //= require_tree ./lib
 //= require_tree ./app
