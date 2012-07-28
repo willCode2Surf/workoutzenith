0889fcb434017c5c838b21bb26aef4ab17901f80
Paul Cowan <dagda1@scotalt.net>
2012-07-28 14:50:03 +0100

    constantize and send used in applicationconroller index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,11 +18,16 @@ class ApplicationController < ActionController::Base
      self.class.to_s.match(/(.+)Controller/)[1].pluralize.underscore
   end
 
+  def resource_class
+    self.class.to_s.match(/(.+)Controller/)[1].singularize.constantize
+  end
+
   def resource_name
     self.class.to_s.match(/(.+)Controller/)[1].singularize
   end
 
   def collection
-    @collection ||= eval "#{resource_name}.all(sort: [[:name, :asc]])"
+    # @collection ||= eval "#{resource_name}.all(sort: [[:name, :asc]])"
+    @collection ||= resource_class.send(:all, sort: [[:name, :asc]])
   end
 end
